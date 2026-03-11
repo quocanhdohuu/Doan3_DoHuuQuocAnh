37f4c12b263bb6787b336ff16263a3d0693dfb1a
quocanhvn <quocanhdohuu@gmail.com>
2026-03-11 22:09:03 +0700

    Xây dựng trang Tổng quan

--- FE/fe_doan3/src/Components/Tongquan.jsx
+++ FE/fe_doan3/src/Components/Tongquan.jsx
@@ -5,10 +5,80 @@ class Tongquan extends Component {
     return (
       <>
         <div className="tongquan">
-            Tổng quan
+          <div className="tongquan-top">
+            <h2>Tổng quan</h2>
+            <p>Chào mừng đến với hệ thống quản lý khách sạn</p>
+          </div>
+          <div className="tongquan-mid">
+            <Cards
+              title={"Tổng số phòng"}
+              logo={"fa-regular fa-building"}
+              number={"0"}
+              desc={"Trống: | Đang dùng: "}
+            />
+            <Cards
+              title={"Tổng số phòng"}
+              logo={"fa-regular fa-building"}
+              number={"0"}
+              desc={"Trống: | Đang dùng: "}
+            />
+            <Cards
+              title={"Tổng số phòng"}
+              logo={"fa-regular fa-building"}
+              number={"0"}
+              desc={"Trống: | Đang dùng: "}
+            />
+            <Cards
+              title={"Tổng số phòng"}
+              logo={"fa-regular fa-building"}
+              number={"0"}
+              desc={"Trống: | Đang dùng: "}
+            />
+          </div>
+          <div className="tongquan-low">
+            <div className="activity">
+              <h3>Trạng thái phòng</h3>
+              <div className="act phongtrong">
+                <p className="green">Phòng trống</p>
+                <span>0</span>
+              </div>
+              <div className="act dangsudung">
+                <p className="blue">Đang sử dụng</p>
+                <span>0</span>
+              </div>
+              <div className="act candondep">
+                <p className="orange">Cần dọn dẹp</p>
+                <span>0</span>
+              </div>
+            </div>
+            <div className="activity">
+              <h3>Thông tin khách</h3>
+              <div className="act">
+                <p>Tổng khách hàng</p>
+                <span>0</span>
+              </div>
+              <div className="act">
+                <p>Đang lưu trú</p>
+                <span>0</span>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
   }
 }
+function Cards({ title, logo, number, desc }) {
+  return (
+    <>
+      <div className="card">
+        <h3>{title}</h3>
+        <i className={logo}></i>
+        <p className="number">{number}</p>
+        <span>{desc}</span>
+      </div>
+    </>
+  );
+}
+
 export default Tongquan;
